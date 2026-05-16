1dbdacbf9517665fea5857a53edd9dbc60d398dd
13607fatima <13607@holbertonstudents.com>
2026-05-16 12:01:54 +0000

    task 6

---
+++ linux_security/0x03_protocols_servers/6-smtp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+level=$(postconf -h smtpd_tls_security_level 2>/dev/null)
+if [ -z "$level" ] || [ "$level" = "none" ]; then
+    echo "STARTTLS not configured"
+else
+    postconf smtpd_tls_security_level
+fi
